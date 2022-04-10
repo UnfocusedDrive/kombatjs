c0d5eec4c7af807b1f3485f5c4bf73d076db7fa8
John Yagiz <john.yagiz@gmail.com>
2022-04-09 23:43:58 -0700

    auto deploy

--- src/util/common.ts
+++
@@ -1,6 +0,0 @@
-export default {
-  // isDOM: (value: HTMLElement) => value instanceof Element,
-  // isObject: value => typeof value === 'object',
-  // isStr: value => typeof value === 'string',
-  // camel2Kebab: string => string.split('').map(s => s === s.toUpperCase() ? `-${s.toLowerCase()}` : s).join('')
-};
